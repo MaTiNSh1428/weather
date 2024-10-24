fbf3f10e5c6ab091df81ae6dc51f9dca91b01da3
MaTiNSh1428 <162770471+MaTiNSh1428@users.noreply.github.com>
2024-10-24 12:11:00 +0000

    upada

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-const apiKey = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/iran?unitGroup=us&key=VSRHEL89K87LGNC5SXTDV6FC9&contentType=json'; // جایگزینی با کلید API Visual Crossing
+const apiKey = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/iran?unitGroup=us&key=VSRHEL89K87LGNC5SXTDV6FC9&contentType=json';
+
+function getWeather() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+
+            document.getElementById('location').innerText = `Latitude: ${lat}, Longitude: ${lon}`;
+
+            fetchWeather(lat, lon);
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
 
 function fetchWeather(lat, lon) {
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}?unitGroup=metric&key=${apiKey}`;
